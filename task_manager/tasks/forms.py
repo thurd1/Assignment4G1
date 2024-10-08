6a3b0b7530ccbfd9e65446ec2adbcfb7f0d0ca44
Trenton <97726179+thurd1@users.noreply.github.com>
2024-10-08 12:24:30 -1000

    Add files via upload

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,10 +1,20 @@
+# tasks/forms.py
 from django import forms
 from .models import Task
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'due_date', 'category', 'completed']
         widgets = {
-            'due_date': forms.DateInput(attrs={'type': 'date'}),  # This will trigger the date picker
+            'title': forms.TextInput(attrs={'placeholder': 'Task Title'}),
+            'due_date': forms.DateInput(attrs={'type': 'date'}),
         }
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email' , 'password1' , 'password2']
--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,20 +1,19 @@
+# tasks/models.py
 from django.db import models
-from django.contrib.auth.models import User
 
 class Task(models.Model):
     CATEGORY_CHOICES = [
         ('Work', 'Work'),
         ('Personal', 'Personal'),
         ('Shopping', 'Shopping'),
-        ('Health', 'Health'),
-	('School', 'School'),
+        ('School', 'School'),
         ('Other', 'Other'),
     ]
 
     title = models.CharField(max_length=200)
     due_date = models.DateField()
-    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
-    completed = models.BooleanField(default=False)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    completed = models.BooleanField(default=False)  
 
     def __str__(self):
         return self.title
